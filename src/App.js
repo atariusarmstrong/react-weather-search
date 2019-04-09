c53e908015bb56aa70639862281a5d60fffaa41c
Atarius Armstrong <atariusarmstrong@gmail.com>
2019-04-09 14:17:12 -0400

    able to update state through api call

--- src/App.js
+++ src/App.js
@@ -2,20 +2,40 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Form from './components/Form';
+import Weather from './components/Weather';
 
 const API_KEY = process.env.REACT_APP_WEATHER_API_KEY
 
 class App extends Component {
+  state = {
+    city: '',
+    country: '',
+    temperature: 0,
+    humidity: 0,
+    description: '',
+    error: undefined
+  }
   getWeather = async (e) => {
     e.preventDefault()
-    const api_call = await fetch (`https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_KEY}`);
+    const city = e.target.elements.city.value
+    const country = e.target.elements.country.value
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
     const data = await api_call.json();
     console.log(data)
+    this.setState({
+      temperature: data.main.temp,
+      city: data.name,
+      country: data.sys.country,
+      humidity: data.main.humidity,
+      description: data.weather[0].description,
+      error: ''
+    })
   }
   render() {
     return (
       <div className="App">
         <Form getWeather={this.getWeather}/>
+        <Weather />
       </div>
     );
   }
