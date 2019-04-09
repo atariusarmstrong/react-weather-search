4c69756f6ec9d85aa1fb6ba423dee011d87e01ed
Atarius Armstrong <atariusarmstrong@gmail.com>
2019-04-09 15:08:51 -0400

    changed to imperial units and installed bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+// import './App.css';
 import Form from './components/Form';
 import Weather from './components/Weather';
 
@@ -19,7 +20,7 @@ class App extends Component {
     e.preventDefault()
     const city = e.target.elements.city.value
     const country = e.target.elements.country.value
-    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=imperial`);
     const data = await api_call.json();
     console.log(data)
     if (city && country) {
